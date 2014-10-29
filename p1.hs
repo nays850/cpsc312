41c831ab7c64e82c85baa7d0057a54ea23b08fe2
nays850 <nays850@gmail.cm>
2014-10-28 20:38:33 -0700

    transpose_right

--- p1.hs
+++ p1.hs
@@ -1,4 +1,5 @@
 
+
 parser :: String -> Int -> Int -> [String]
 parser inlist n i
  | i < n - 1 = (parser_take inlist (n + i)) : (parser (drop (n + i) inlist) n (i+1) )
@@ -42,4 +43,28 @@ transpose_drop inlist start end
  | start /= 0 = (head inlist):(transpose_drop (tail inlist) (start - 1) end) 
  | end >= 0   = (drop_last (head inlist))
 		:(transpose_drop (tail inlist) start (end - 1))
+ | otherwise = inlist
+
+
+transpose_right :: [String] -> Int -> Int -> [String]
+transpose_right inlist n i
+ | i < n = (transpose_ind_r inlist 0 (n + i - 2) ) 
+	:(transpose_right (transpose_drop_r inlist 0 (n + i - 2)) n (i+1))
+ | i <= 2*n - 1 = (transpose_ind_r inlist (i - n) (2*n - 2)) 
+	:(transpose_right (transpose_drop_r inlist (i - n) (2*n - 2)) n (i+1))
+ | otherwise = []
+
+transpose_ind_r :: [String] -> Int -> Int -> String
+transpose_ind_r inlist start end 
+ | null inlist = []
+ | start /= 0 = (transpose_ind_r (tail inlist) (start - 1) end)
+ | end >= 0   = (head (head inlist)) : (transpose_ind_r (tail inlist) start (end - 1))
+ | otherwise = []
+
+transpose_drop_r :: [String] -> Int -> Int -> [String]
+transpose_drop_r inlist start end
+ | null inlist = inlist
+ | start /= 0 = (head inlist):(transpose_drop_r (tail inlist) (start - 1) end) 
+ | end >= 0   = (tail (head inlist))
+		:(transpose_drop_r (tail inlist) start (end - 1))
  | otherwise = inlist
